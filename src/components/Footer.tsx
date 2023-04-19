d957c390fc95519df3b3fc29bfce41a2b6761dab
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-04-19 10:14:15 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -44,7 +44,7 @@ export const Footer = () => {
           <div className="h-1 rounded-full w-96 bg-zinc-600">
             <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
           </div>
-          <span className="text-xs text-zinc-400">2:14</span>
+          <span className="text-xs text-zinc-400">3:49</span>
         </div>
       </div>
       <div className="flex items-center gap-4">
